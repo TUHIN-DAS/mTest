4afce7334d292e37398cba7db7436e0e9d26f28a
nikisak <niks.jadwa@gmail.com>
2018-12-24 16:49:40 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { MTLoginComponent } from './m-tlogin/m-tlogin.component';
 import { MTDashboardComponent } from './m-tdashboard/m-tdashboard.component';
 import { MTCodingOnlineComponent } from './m-tcoding-online/m-tcoding-online.component';
 import { MTRegisterComponent } from './m-tregister/m-tregister.component';
+import { AuthGuardService } from './services/authguard.service';
+
 const routes: Routes = [
   { path: '', redirectTo: '/mTLogin', pathMatch: 'full' },
   { path: 'mTLogin',component: MTLoginComponent },
-  { path: 'mTDashboard',component:MTDashboardComponent},
+  { path: 'mTDashboard',component:MTDashboardComponent,canActivate: [AuthGuardService]},
   { path: 'mTCodingOnline',component:MTCodingOnlineComponent},
   { path: 'mTRegister',component:MTRegisterComponent}
 ];
